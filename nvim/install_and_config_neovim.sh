1502ca977316d70b96053e94858d9440a6bebee7
Emil Sahakyan <emamajanyan@gmail.com>
2022-08-03 21:19:16 +0400

    Update

--- nvim/install_and_config_neovim.sh
+++ nvim/install_and_config_neovim.sh
@@ -3,42 +3,67 @@
 # Remove existing nvim installation and configs
 sudo rm /usr/local/bin/nvim
 sudo rm -r /usr/local/share/nvim/
-rm -r ~/.config/nvim 
+rm -rf ~/.config/nvim 
 rm -rf ~/.local/share/nvim/
 
 # install neovim from source
 # Prerequsites: https://github.com/neovim/neovim/wiki/Building-Neovim#build-prerequisites
+    # Ubuntu / Debian
+    # sudo apt install -y  install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+    # sudo apt install -y font-manager 
 git clone https://github.com/neovim/neovim.git ~/.config/nvim/neovim
-cd ~/.config/nvim/neovim
+pushd ~/.config/nvim/neovim
 git checkout stable
 make -j `nproc` CMAKE_BUILD_TYPE=Release
 sudo make install
+echo "DONE installing nvim"
 
+popd
 # Cleanup
-rm -rf ~/.config/nvim/neovim
-git clone https://github.com/emilsahakyan/nvim.git ~/.config/nvim/
+rm -rf ~/.config/nvim
+echo "DONE clean up"
+
+# AstroNvim installation
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+echo "CLONED AstroNvim"
+# My Nvim configuration
+git clone https://github.com/emilsahakyan/nvim.git ~/.config/nvim/lua/user
+echo "CLONED config"
+
+# Install Nord fonts
+FDIR="/home/$USER/.local/share/fonts"
+pushd ~/.config/nvim/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip .
+unzip Ubuntu.zip
+mkdir -p $FDIR 
+mv Ubuntu/* $FDIR 
+rm -r Ubuntu Ubuntu.zip 
+
+# For language servers
+
+sudo apt install -y  python3-venv npm
+# Lsp servers
 nvim  --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim +LspInstall pyright
-nvim +LspInstall clangd
-nvim +LspInstall cmake
-nvim +LspInstall jsonls
-nvim +LspInstall pyright
-nvim +LspInstall sumneko_lua
-nvim +LspInstall verible
+nvim  --headless -c 'LspInstall pyright'
+nvim  --headless -c 'LspInstall clangd'
+nvim  --headless -c 'LspInstall cmake'
+nvim  --headless -c 'LspInstall jsonls'
+nvim  --headless -c 'LspInstall sumneko_lua'
+nvim  --headless -c 'LspInstall verible'
 
-nvim :TSInstall python
-nvim :TSInstall bash
-nvim :TSInstall c
-nvim :TSInstall cmake
-nvim :TSInstall cpp
-nvim :TSInstall help
-nvim :TSInstall make
-nvim :TSInstall python
-nvim :TSInstall regex
-nvim :TSInstall verilog
-nvim :TSInstall vim
+# Treesitter
+nvim  --headless -c 'TSInstall python'
+nvim  --headless -c 'TSInstall bash'
+nvim  --headless -c 'TSInstall c'
+nvim  --headless -c 'TSInstall cmake'
+nvim  --headless -c 'TSInstall cpp'
+nvim  --headless -c 'TSInstall help'
+nvim  --headless -c 'TSInstall make'
+nvim  --headless -c 'TSInstall python'
+nvim  --headless -c 'TSInstall regex'
+nvim  --headless -c 'TSInstall verilog'
+nvim  --headless -c 'TSInstall vim'
 
-
-# For debugging python scrips using DAP
+# Uncomment for debugging python scrips using DAP
 python -m venv /home/$USER/python/virtualenvs/debugpy
 /home/$USER/python/virtualenvs/debugpy/bin/python -m pip install debugpy
