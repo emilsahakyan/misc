5f7314637ea4c14bb7917fd9ebc768476616d701
Emil Sahakyan <emamajanyan@gmail.com>
2022-08-04 17:31:35 +0400

    Separate install and config scripts

--- nvim/config.sh
+++ nvim/config.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
 
-# Remove existing nvim installation and configs
-sudo rm /usr/local/bin/nvim
-sudo rm -r /usr/local/share/nvim/
-sudo rm -rf ~/.config/nvim 
-sudo rm -rf ~/.local/share/nvim/
-
-# install neovim from source
-# Prerequsites: https://github.com/neovim/neovim/wiki/Building-Neovim#build-prerequisites
-# Ubuntu / Debian
-sudo apt install -y  ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen pip
-# To install fonts
-sudo apt install -y font-manager 
-# For language servers
- sudo apt install -y  python3-venv
-# sudo apt install -y  npm
-git clone https://github.com/neovim/neovim.git ~/.config/nvim/neovim
-pushd ~/.config/nvim/neovim
-git checkout stable
-make -j `nproc` CMAKE_BUILD_TYPE=Release
-sudo make install
-popd
-
-# Cleanup
-sudo rm -rf ~/.config/nvim
+# Remove any old config files
+rm -rf /home/$USER/.config/nvim 
+rm -rf /home/$USER/.local/share/nvim/
 
 # AstroNvim installation
-git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+git clone https://github.com/AstroNvim/AstroNvim /home/$USER/.config/nvim
 # My Nvim configuration
-git clone https://github.com/emilsahakyan/nvim.git ~/.config/nvim/lua/user
+git clone https://github.com/emilsahakyan/nvim.git /home/$USER/.config/nvim/lua/user
 
 # Install Nord fonts
 FDIR="/home/$USER/.local/share/fonts"
-pushd ~/.config/nvim/
+pushd /home/$USER/.config/nvim/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip .
 unzip Ubuntu.zip
 mkdir -p $FDIR 
---
+++ nvim/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Remove existing nvim installation and configs
+sudo rm /usr/local/bin/nvim
+sudo rm -r /usr/local/share/nvim/
+
+# install neovim from source
+# Prerequsites: https://github.com/neovim/neovim/wiki/Building-Neovim#build-prerequisites
+# Ubuntu / Debian
+sudo apt install -y  ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen pip
+# To install fonts
+sudo apt install -y font-manager 
+# For language servers
+sudo apt install -y  python3-venv
+# sudo apt install -y  npm
+git clone https://github.com/neovim/neovim.git /home/$USER/neovim
+pushd /home/$USER/neovim
+git checkout stable
+make -j `nproc` CMAKE_BUILD_TYPE=Release
+sudo make install
+popd
+sudo rm -rf /home/$USER/neovim
